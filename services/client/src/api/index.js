5a3bf542f1bcd2ee45e4349b987fac8b2a0fd364
Dacio Romero <dacioromero@gmail.com>
2019-03-22 02:17:34 -0700

    Fix param serializer

--- services/client/src/api/index.js
+++ services/client/src/api/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import qs from 'qs';
+// import flip from 'lodash/flip';
+// import wrap from 'lodash/wrap';
 
-const paramsSerializer = qs.stringify;
+// const paramsSerializer = wrap({ indices: false }, flip(qs.stringify));
+const paramsSerializer = (params) => qs.stringify(params, { indices: false })
 
 const resToData = res => res.data;
 
