8abff9c9831410ab671d500126727e596fa94084
Dacio Romero <dacioromero@gmail.com>
2019-03-26 16:21:18 -0700

    Store glossaries in Redis

--- services/server/controllers/glossaries.js
+++ services/server/controllers/glossaries.js
@@ -1,65 +1,56 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+
 const keyBy = require('lodash/keyBy');
+const zipObject = require('lodash/zipObject');
 
 const IGDB = require('../utils/igdb');
+const cache = require('../utils/redis');
 
 const router = express.Router();
 
 const extrResData = res => res.data;
 const keyArrById = arr => keyBy(arr, 'id');
 
-let cache = null;
-const resCache = (req, res, next) => {
-  if (cache) {
-    res.json(cache);
-  } else {
-    next();
+router.get('/', asyncHandler(async (req, res) => {
+  let glossaries = JSON.parse(await cache.getAsync('/glossaries'));
+
+  if (!glossaries) {
+    glossaries = zipObject(
+      ['themes', 'genres', 'player_perspectives', 'platforms', 'game_modes'],
+      await Promise.all([
+        IGDB()
+          .fields('name')
+          .request('/themes')
+          .then(extrResData)
+          .then(keyArrById),
+        IGDB()
+          .fields('name')
+          .request('/genres')
+          .then(extrResData)
+          .then(keyArrById),
+        IGDB()
+          .fields('name')
+          .request('/player_perspectives')
+          .then(extrResData)
+          .then(keyArrById),
+        IGDB()
+          .fields('name')
+          .where('id=(3,6,14)')
+          .request('/platforms')
+          .then(extrResData)
+          .then(keyArrById),
+        IGDB()
+          .fields('name')
+          .request('/game_modes')
+          .then(extrResData)
+          .then(keyArrById),
+      ]),
+    );
+    await cache.setAsync('/glossaries', JSON.stringify(glossaries));
   }
-};
 
-router.get('/', resCache, asyncHandler(async (req, res) => {
-  const [
-    // eslint-disable-next-line camelcase
-    themes, genres, player_perspectives, platforms, game_modes,
-  ] = await Promise.all([
-    IGDB()
-      .fields('name')
-      .request('/themes')
-      .then(extrResData)
-      .then(keyArrById),
-    IGDB()
-      .fields('name')
-      .request('/genres')
-      .then(extrResData)
-      .then(keyArrById),
-    IGDB()
-      .fields('name')
-      .request('/player_perspectives')
-      .then(extrResData)
-      .then(keyArrById),
-    IGDB()
-      .fields('name')
-      .where('id=(3,6,14)')
-      .request('/platforms')
-      .then(extrResData)
-      .then(keyArrById),
-    IGDB()
-      .fields('name')
-      .request('/game_modes')
-      .then(extrResData)
-      .then(keyArrById),
-  ]);
-
-  cache = {
-    themes,
-    genres,
-    player_perspectives,
-    platforms,
-    game_modes,
-  };
-
-  res.json(cache);
+  res.json(glossaries);
 }));
 
 module.exports = router;
