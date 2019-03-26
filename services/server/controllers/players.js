4cad723ea32d4523f0b826a0df9234e3e6ac28a0
Dacio Romero <dacioromero@gmail.com>
2019-03-26 15:38:40 -0700

    Cache Steam results Redis

--- services/server/controllers/players.js
+++ services/server/controllers/players.js
@@ -2,12 +2,79 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const zipObject = require('lodash/zipObject');
+const isEmpty = require('lodash/isEmpty');
 const merge = require('lodash/merge');
+const mapValues = require('lodash/mapValues');
 
-const SteamWrapper = require('steam-wrapper');
+const cache = require('../utils/redis');
 
 const router = express.Router();
-const Steam = new SteamWrapper(process.env.STEAM_API_KEY);
+const Steam = require('../utils/steam');
+
+// Force Redis to store false for null and convert back; null is expected for no response
+const convertFalsy = forced => v => v || forced;
+const convFalsyNull = convertFalsy(null); // Convert cache to wrapper
+const convFalsyFalse = convertFalsy(false); // Wrapper to cache
+
+const getLibraries = async (ids) => {
+  // Get cached values
+  const cachedLibs = zipObject(
+    ids,
+    (await cache.mgetAsync(...ids.map(id => `/libraries/${id}`)))
+      .map(JSON.parse)
+      .map(convFalsyNull),
+  );
+
+  // Get from wrapper new for all missing
+  const newLibs = Object.assign(
+    {},
+    ...(await Promise.all(Object.entries(cachedLibs)
+      .filter(([, games]) => games === null)
+      .map(async ([id]) => {
+        const games = await Steam.GetOwnedGames(id);
+        return { [id]: games };
+      }))),
+  );
+
+  // Store missing values
+  if (!isEmpty(newLibs)) {
+    await cache.msetAsync(
+      ...Object.entries(newLibs)
+        .map(([id, games]) => [`/libraries/${id}`, JSON.stringify(convFalsyFalse(games))])
+        .flat(),
+    );
+  }
+
+  return { ...cachedLibs, ...newLibs };
+};
+
+const getProfiles = async (ids) => {
+  // Get cached values
+  const cachedProfs = zipObject(
+    ids,
+    (await cache.mgetAsync(...ids.map(id => `/profiles/${id}`)))
+      .map(JSON.parse)
+      .map(convFalsyNull),
+  );
+
+  // Get from wrapper new for all missing
+  const newProfs = await Steam.GetPlayerSummaries(
+    ...Object.entries(cachedProfs)
+      .filter(([, profile]) => profile === null)
+      .map(([id]) => id),
+  );
+
+  // Store missing values
+  if (!isEmpty(newProfs)) {
+    await cache.msetAsync(
+      ...Object.entries(newProfs)
+        .map(([id, profile]) => [`/profiles/${id}`, JSON.stringify(convFalsyFalse(profile))])
+        .flat(),
+    );
+  }
+
+  return { ...cachedProfs, ...newProfs };
+};
 
 router.get('/', asyncHandler(async (req, res) => {
   let { steamIds: ids } = req.query;
@@ -16,14 +83,15 @@ router.get('/', asyncHandler(async (req, res) => {
     ids = ids.split(',');
   }
 
+  // Convert URLs, IDs, and vanity names
   ids = (await Promise.all(ids.map(id => Steam.GetSteamId64(id)))).filter(Boolean);
 
   const [libraries, profiles] = await Promise.all([
-    Promise.all(ids.map(id => Steam.GetOwnedGames(id))),
-    Steam.GetPlayerSummaries(...ids),
+    getLibraries(ids),
+    getProfiles(ids),
   ]);
 
-  merge(profiles, zipObject(ids, libraries.map(games => ({ games }))));
+  merge(profiles, mapValues(libraries, games => ({ games })));
 
   res.json(profiles);
 }));
---
+++ services/server/utils/steam.js
@@ -0,0 +1,3 @@
+const SteamWrapper = require('steam-wrapper');
+
+module.exports = new SteamWrapper(process.env.STEAM_API_KEY);;
