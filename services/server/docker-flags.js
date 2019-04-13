3796615bf18de33eecf00302b4ce65b67b7bfea8
Dacio Romero <dacioromero@gmail.com>
2019-04-13 12:36:22 -0700

    Simplify Docker flag generator

--- services/server/docker-flags.js
+++ services/server/docker-flags.js
@@ -1,10 +1,9 @@
 // For creating Docker build flags based on semver
-const imageName = process.argv[2];
-const fullVersion = process.argv[3];
+const [, , imageName, fullVersion] = process.argv;
 
-const versions = [
-  ...fullVersion.split('.').map((v, i, arr) => arr.slice(0, i + 1).join('.')),
-  'latest',
-];
+const versions = fullVersion
+  .split('.')
+  .map((v, i, arr) => arr.slice(0, i + 1).join('.'))
+  .concat('latest');
 
 console.log(versions.map(version => `-t ${imageName}:${version}`).join(' '));
