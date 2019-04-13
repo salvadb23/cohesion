af0e0f5c70ad2f4c870fe9079390afb083e7d827
Dacio Romero <dacioromero@gmail.com>
2019-04-13 05:11:53 -0700

    Use split, join, and splice over regex in docker-flags

--- services/server/docker-flags.js
+++ services/server/docker-flags.js
@@ -1,9 +1,10 @@
 // For creating Docker build flags based on semver
-const versionRe = /((\d+)\.\d+)\.\d+/;
-
 const imageName = process.argv[2];
 const fullVersion = process.argv[3];
 
-const versions = [...versionRe.exec(fullVersion), 'latest'];
+const versions = [
+  ...fullVersion.split('.').map((v, i, arr) => arr.slice(0, i + 1).join('.')),
+  'latest',
+];
 
 console.log(versions.map(version => `-t ${imageName}:${version}`).join(' '));
