9af3f27a5a53d3e0a6cc382e13846190be11a2a6
Dacio Romero <dacioromero@gmail.com>
2019-07-30 00:28:37 -0700

    Convert to monorepo further

--- services/client/docker-flags.js
+++
@@ -1,9 +0,0 @@
-// For creating Docker build flags based on semver
-const versionRe = /((\d+)\.\d+)\.\d+/;
-
-const imageName = process.argv[2];
-const fullVersion = process.argv[3];
-
-const versions = [...versionRe.exec(fullVersion), 'latest'];
-
-console.log(versions.map(version => `-t ${imageName}:${version}`).join(' '));
--- services/server/docker-flags.js
+++
@@ -1,9 +0,0 @@
-// For creating Docker build flags based on semver
-const [, , imageName, fullVersion] = process.argv;
-
-const versions = fullVersion
-  .split('.')
-  .map((v, i, arr) => arr.slice(0, i + 1).join('.'))
-  .concat('latest');
-
-console.log(versions.map(version => `-t ${imageName}:${version}`).join(' '));
